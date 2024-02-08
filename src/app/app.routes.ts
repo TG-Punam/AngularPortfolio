05c54015ddd0676d037b6b91a4c319dc99a856f6
TG-Punam <punam.s@truglobal.com>
2024-02-08 18:03:22 +0530

    Appone

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LiveComponent } from './pages/live/live.component';
+import { PointTableComponent } from './pages/point-table/point-table.component';
+import { HistoryComponent } from './pages/history/history.component';
+import { EraListComponent } from './pages/era-list/era-list.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'home',
+        component: HomeComponent,
+        title: 'Home | CrickInformer',
+    },
+    {
+        path: 'projects',
+        component: ProjectsComponent,
+        title: 'My Projects',
+    },
+    {
+        path:'history',
+        component: HistoryComponent,
+        title:'Match History | CrickInformer',
+    },
+    {
+        path:'live',
+        component: LiveComponent, 
+        title: 'Live Matches | CrickInformer '
+    },
+    {
+        path:'point-table',
+        component:PointTableComponent,
+        title:'Point Table'
+    },
+    {
+        path: 'era-list',
+        component: EraListComponent,
+        title: 'Live | Era Gaming',
+    }
+];
